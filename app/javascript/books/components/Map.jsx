42ce49de0a91b3c685f9663e6c0407b4b841dc59
Jane <iamjane.gmail.com>
2017-12-29 17:22:41 -0600

    somethigns wrong with the manifest.json

---
+++ app/javascript/books/components/Map.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import queryString from 'query-string';
+import axios from 'axios';
+
+class Map extends React.Component {
+  constructor (){
+    super();
+    this.state={
+      book: {}
+    };
+  }
+
+  fetchBook (id) {
+    axios.get(`api/books/${id}`)
+    .then(response =>{
+      this.setState({book: response.data});
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }
+
+  setBookIdFromQueryString (qs) {
+    this.qsParams = queryString.parse(qs);
+    if(this.qsParams.book) {
+      this.bookId = Number(this.qsParams.book);
+    } else {
+      this.bookId = 1;
+      this.props.history.push(`/?quote=${this.bookId}`);
+    }
+  }
+
+  componentDidMount(){
+    this.setBookIdFromQueryString(this.props.location.search);
+    this.fetchBook(this.bookId);
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setBookIdFromQueryString(nextProps.location.search);
+    this.fetchBook(this.bookId);
+  }
+
+  render(){
+    const nextBookId = Number(this.state.book.id) + 1;
+    return(
+      <div>
+        <Link to={`/?quote=${nextQuoteId}`}>Next</Link>
+        <p>{this.state.book.title}</p>
+        <p>{this.state.book.author}</p>
+      </div>
+      );
+  }
+
+}
+
+export default Map
---
+++ app/javascript/books/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+const books = document.querySelector('#books')
+ReactDOM.render(<App />, books)
