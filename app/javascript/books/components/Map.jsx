59cb187094c4c34885cfef55dfed6deace35e7a6
Jane <iamjane.gmail.com>
2017-12-29 19:28:01 -0600

    Add map with clickable countries

--- app/javascript/books/components/Map.jsx
+++ app/javascript/books/components/Map.jsx
@@ -1,57 +1,68 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import queryString from 'query-string';
-import axios from 'axios';
+import React, {Component} from 'react';
+import  {geoMercator, geoPath} from 'd3-geo'
+import {feature} from 'topojson-client'
+// import {Link} from 'react-router-dom';
+// import queryString from 'query-string';
+// import axios from 'axios';
 
-class Map extends React.Component {
-  constructor (){
+class Map extends Component {
+
+  constructor(){
     super();
-    this.state={
-      book: {}
-    };
-  }
-
-  fetchBook (id) {
-    axios.get(`api/books/${id}`)
-    .then(response =>{
-      this.setState({book: response.data});
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  }
-
-  setBookIdFromQueryString (qs) {
-    this.qsParams = queryString.parse(qs);
-    if(this.qsParams.book) {
-      this.bookId = Number(this.qsParams.book.id);
-    } else {
-      this.bookId = 1;
-      this.props.history.push(`/?book=${this.bookId}`);
+    this.state = {
+      worldData: [],
     }
+    this.handleCountryClick = this.handleCountryClick.bind(this)
+  }
+
+  handleCountryClick(countryIndex) {
+    console.log("Clicked on a country: ", this.state.worldData[countryIndex])
+  }
+
+  projection() {
+    return geoMercator()
+    .scale(100)
+    .translate([800/2, 450/2])
   }
 
   componentDidMount(){
-    this.setBookIdFromQueryString(this.props.location.search);
-    this.fetchBook(this.bookId);
-  }
-
-  componentWillReceiveProps(nextProps){
-    this.setBookIdFromQueryString(nextProps.location.search);
-    this.fetchBook(this.bookId);
+    fetch("/world-110m.json")
+    .then(response => {
+      if(response.status !== 200){
+        console.log(`Oh noes! ${response.status}`)
+        return
+      }
+      response.json().then(worldData=>{
+        this.setState({
+          worldData:feature(worldData, worldData.objects.countries).features,
+        })
+      })
+    })
   }
 
   render(){
-    const nextBookId = Number(this.state.book.id) + 1;
-    return(
-      <div>
-        <Link to={`/?book=${nextBookId}`}>Next</Link>
-        <p>{this.state.book.title}</p>
-        <p>{this.state.book.author}</p>
-      </div>
-      );
-  }
 
+    return(
+      <svg width={800} height={450} viewBox="0 0 800 450">
+        <g className="countries">
+          {
+            this.state.worldData.map((d, i)=> (
+              <path
+                key = { `path-${i}`}
+                d = { geoPath().projection(this.projection())(d) }
+                className="country"
+                onClick = { () => this.handleCountryClick(i)}
+                fill= {`rgba(38,50,56, ${1/this.state.worldData.length * i})`}
+                stroke= "#FFFFFF"
+                strokeWidth= { 0.5}
+              />
+            ))
+          }
+        </g>
+
+      </svg>
+      )
+  }
 }
 
 export default Map
