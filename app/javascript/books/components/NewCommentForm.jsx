098c063004950d199e3b4cc3ce39a4141635cad2
Jane <iamjane.gmail.com>
2018-01-13 14:35:40 -0800

    comments showing up on correct book page

---
+++ app/javascript/books/components/NewCommentForm.jsx
@@ -0,0 +1,98 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+
+class NewBookForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      book_id: this.props.book_id,
+      comment:'',
+      user_id: null,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  getComments(){
+    let that = this
+    axios.get('/booklists', {
+
+    })
+    .then(function(response){
+      console.log("GET COMMENTS RESPONSE", response)
+      console.log("GET COMMENTS RESPONSE.DATA", response.data)
+      that.setState({
+        comments:response.data
+      })
+    })
+    .catch(function(error){
+      console.log(error)
+    })
+  }
+
+  componentDidMount(){
+    this.getComments()
+    let that = this
+    axios.get('/pages/are_we_there_yet',{
+    })
+    .then(function(response){
+      if(response.data.email){
+        that.setState({
+          user_id: response.data.id
+        })
+      }
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+  }
+
+
+
+  handleSubmit(e){
+    console.log("Book!", this.state.book_id)
+    e.preventDefault();
+    let that = this.state
+    let t_h_i_s = this
+    axios.post('/booklists',{
+        user_id: that.user_id,
+        book_id: that.book_id,
+        comment: that.comment
+    })
+    .then(function(response){
+      t_h_i_s.getComments();
+      console.log(response);
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+  }
+
+    handleInputChange(e){
+    let target = e.target;
+    let name = target.name
+    let value = target.value
+    this.setState({
+      [name]: value
+    })
+  }
+
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+          <fieldset>
+            <label>
+            <h3>Add a Comment</h3>
+              <input name="comment" placeholder="comment" type="textbox" defaultValue="" onChange={this.handleInputChange}/> <br/>
+              <input type="submit" onChange={this.handleSubmit} />
+            </label>
+          </fieldset>
+        </form>
+      )
+  }
+}
+
+export default NewBookForm
--- app/javascript/books/components/Search.jsx
+++ app/javascript/books/components/Search.jsx
@@ -4,9 +4,20 @@ import Header from './Header';
 
 import Searchbar from './containers/searchbar';
 import ListOfBooks from './containers/list_of_books'
+import NewBookForm from './NewBookForm'
 
 
 class Search extends React.Component {
+
+  addbook(){
+    if(this.props.currentUser != null){
+      return(
+          <NewBookForm
+            currentUser={this.props.currentUser}
+          />
+        )
+    }
+  }
 
   render(){
     return(
@@ -15,6 +26,7 @@ class Search extends React.Component {
             currentUser={this.props.currentUser}
             updateCurrentUser={this.props.updateCurrentUser}
         />
+        {this.addbook()}
         <h4>Search Books By Country:</h4> <Searchbar/>
         <br/>
         <ListOfBooks/>
